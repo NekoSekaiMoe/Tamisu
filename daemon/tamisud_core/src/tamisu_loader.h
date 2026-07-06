3d26c7335b16156c915800af6cae33e241a8d486
root <dabao1955@163.com>
2026-07-06 10:52:50 +0800

    Reapply "tmp"
    This reverts commit 92e49a83ae5f456f282dbdf65e773d3502b6235f.

--- daemon/tamisud_core/src/tamisu_loader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace tamisu_daemon::tamisu_loader {
-
-bool load_module(const char* path, const std::string& param_values = "");
-
-}  // namespace tamisu_daemon::tamisu_loader
